b93b9ff71cd1421988bb1c2efa6d9d29c705a8c1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-06 12:11:44 -0200

    Criando a configuração do banco de dados.

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Arquivo: src/config/database.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável pela configuração do banco de dados.
+ * Data: 06/02/2018
+ *
+ */
+
+import mongoose, { Promise } from 'mongoose';
+mongoose.Promise = Promise;
+
+const mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost/sw-api-node-v1';
+
+const connect = () => mongoose.connect(mongodbUrl);
+
+export default { connect }
