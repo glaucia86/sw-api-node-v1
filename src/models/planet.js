aca90af436f58f9aeb486b148a62234519835873
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-06 12:58:48 -0200

    Desenvolvimento da classe modelo no arquivo 'planet.js'

---
+++ src/models/planet.js
@@ -0,0 +1,33 @@
+/**
+ *
+ * Arquivo: src/models/planet.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável pelo modelo: 'Planet' 
+ * Data: 06/02/2018
+ *
+ */
+
+/**
+ * Infos inerentes a classe: 
+ * ==== Planet ====
+ * id: number
+ * nome: string
+ * clima: string
+ * terreno: string
+ */
+
+import mongoose from 'mongoose';
+
+const PlanetSchema = new mongoose.Schema(
+  {
+    nome: { type: String, required: true },
+    clima: { type: String, required: true },
+    terreno: { type: String, required: true }
+  },
+  {
+    versionKey: false
+  });
+
+const Planet = mongoose.model('Planet', PlanetSchema);
+
+export default Planet;
