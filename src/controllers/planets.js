9d7933a5ebef9a3d122526083e960a75caab5867
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-06 11:28:31 -0200

    Desenvolvimento do teste de unidade no controller - planets.js'

---
+++ src/controllers/planets.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * Arquivo: src/controllers/planets.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável por lidar com a lógica dos HTTP's da api.
+ * Data: 06/02/2018
+ *
+ */
+
+class PlanetsController {
+
+}
+
+export default PlanetsController;
---
+++ test/unit/controllers/planets_spec.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Arquivo: test/unit/controllers/planets_spec.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável por receber os testes de unidade de 'Planets' da Api.
+ * Data: 06/02/2018
+ *
+ */
+
+import PlanetsController from '../../../src/controllers/planets';
+
+describe('Controllers: Planets', ()=> {
+    describe('get() /planets', () => {
+        it('Deve retornar uma lista de Planetas', () => {
+            const planetsController = new PlanetsController();
+        });
+    });
+});
